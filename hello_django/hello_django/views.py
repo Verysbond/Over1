2405fde52b5d45d237dba2187c6fcb5d1b824a35
Admin <verysbond@ya.ru>
2023-06-06 12:41:52 +0300

    Merge remote-tracking branch 'origin/master'

--- hello_django/hello_django/views.py
+++ hello_django/hello_django/views.py
@@ -6,6 +6,11 @@ def about(request):
     a = 2 + 3
     return render(request, 'about.html', {'gretting': a})
 
+def reverse(request):
+    user_text = request.GET['usertext']
+    reverse = user_text[::-1]
+    return render(request, 'reverse.html', {'word': reverse})
+
 
 def home(request):
     return render(request, 'home.html')
